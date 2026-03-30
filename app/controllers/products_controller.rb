660c1d83e4c192b188881420b5fd3820eecd7f4b
zdeboer <zackdb2005@gmail.com>
2026-03-30 14:14:34 -0500

    Added Rubocop configuration, added genre dropdown on product search.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,25 @@
 class ProductsController < ApplicationController
   def index
     @q = params[:q].to_s.strip
+    @genres = Genre.order(:name)
+    @genre_id = params[:genre_id].presence
 
     scope = Product.includes(:genre, :media_type)
+
+    if @genre_id.present?
+      scope = scope.where(genre_id: @genre_id)
+    end
+
     if @q.present?
-        escaped = ActiveRecord::Base.sanitize_sql_like(@q)
-        scope = scope.where(
+      escaped = ActiveRecord::Base.sanitize_sql_like(@q)
+      scope = scope.where(
         "products.name LIKE :q OR products.description LIKE :q",
         q: "%#{escaped}%"
-        )
+      )
     end
 
     @products = scope.order(:name)
-    end
+  end
 
   def show
     @product = Product.includes(product_variations: :inventory).find(params[:id])
