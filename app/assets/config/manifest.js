4a636c4f78a683ec268652431de51ca4d600a225
zdeboer <zackdb2005@gmail.com>
2026-03-31 09:32:36 -0500

    Fixing scan_js problems (Just getting rid of it)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
 //= link active_admin.css
-//= link active_admin.js
+//= link active_admin.js//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
