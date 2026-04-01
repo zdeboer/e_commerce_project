edad594f2cd56102cd6073abda80cd14d6f93072
zdeboer <zackdb2005@gmail.com>
2026-04-01 11:38:12 -0500

    Fixed security issue

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,19 @@
 class Page < ApplicationRecord
+  before_save :sanitize_content
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[
       title slug content created_at updated_at
     ]
+  end
+
+  private
+
+  def sanitize_content
+    self.content = ActionController::Base.helpers.sanitize(
+      content,
+      tags:       %w[h1 h2 h3 h4 h5 h6 p br strong em b i u a ul ol li blockquote img],
+      attributes: %w[href src alt title]
+    )
   end
 end
